b2ddafcd9b0591cc25e519332da13ff66ce78eef
manjeet <manjeetbhatia83@gmail.com>
2022-12-27 21:37:17 +0530

    6th commit

--- rate_coeff/home/urls.py
+++ rate_coeff/home/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+from django.contrib import admin
+from . import views, include
 
 urlpatterns = [
-    path('',views.home, name='home')
+#    path('',views.home, name='home')
+    path('admin/', admin.site.urls),
+    path('', include('home.urls'))
 ]
