6cee26b997d92b4257a4c7011c5cb95097df9c2b
manjeet <manjeetbhatia83@gmail.com>
2022-12-28 11:20:57 +0530

    25 commit

--- rate_coeff/home/urls.py
+++ rate_coeff/home/urls.py
@@ -18,7 +18,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home, name='home')
+    path('',views.home, name='home'),
+    path('add', views.add, name='add'),
 ]
 #urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 #urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
