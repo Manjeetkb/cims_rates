24ea0a725e2f1fe8be5f1b8e9f0e105a9b5aed56
manjeet <manjeetbhatia83@gmail.com>
2022-12-28 10:41:13 +0530

    22 commit

--- rate_coeff/home/urls.py
+++ rate_coeff/home/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
 #   path('', include('home.urls')),
 #   path('admin/', admin.site.urls),
    path('', views.home, name='home'),
-   path('add', views.add, name='add'),
+ #  path('add', views.add, name='add'),
 
 ]
